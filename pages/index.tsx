1ce995cb157e63483e52028ad539ecea6c440948
Jessi <jessi.heger@curology.com>
2022-11-28 21:59:20 -0500

    small fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -150,7 +150,7 @@ const Home: NextPage = () => {
           </>
         )}
 
-        {currentPage === Page.VIEW_PASS && mintedNft !== undefined && (
+        {currentPage === Page.VIEW_PASS && mintedNft && mintedNft.name !== undefined &&(
           <ViewPass mintedNft={mintedNft}></ViewPass>
         )}
 
